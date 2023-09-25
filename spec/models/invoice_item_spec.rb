096147b994f5efa40737b54f1ed9eb6ffc92cc11
Robert deLaguna <rjdelaguna@yahoo.com>
2023-09-24 21:38:44 -0700

    feat and test: User Story 7

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -15,6 +15,34 @@ RSpec.describe InvoiceItem, type: :model do
       end
     end
 
+    describe "#price" do
+      it "returns unit_price calculated to dollars" do
+        merchant = Merchant.create!(name: "Bubba's Boutique")
+        spinner = merchant.items.create!(name: "Fidget Spinner", description: "Spins", unit_price: 1)
+        cust = Customer.create!(first_name: "Dave", last_name: "Beckam")
+        invoice = cust.invoices.create!(status: 1)
+        invoice_item = InvoiceItem.create!(item_id: spinner.id, invoice_id: invoice.id, quantity: 20, status: 0, unit_price: 300)
+
+        expect(invoice_item.price).to eq(3.0)
+      end
+    end
+
+    describe "#discount" do
+      it "returns the discount with the highest percentage, which is the discount applied" do
+        merchant = Merchant.create!(name: "Bubba's Boutique")
+        spinner = merchant.items.create!(name: "Fidget Spinner", description: "Spins", unit_price: 1)
+        cust = Customer.create!(first_name: "Dave", last_name: "Beckam")
+        invoice = cust.invoices.create!(status: 1)
+        invoice_item = InvoiceItem.create!(item_id: spinner.id, invoice_id: invoice.id, quantity: 20, status: 0, unit_price: 300)
+        discount1 = BulkDiscount.create!(merchant_id: merchant.id, percentage: 50, threshold: 20)
+        discount2 = BulkDiscount.create!(merchant_id: merchant.id, percentage: 25, threshold: 20)
+
+        expect(invoice_item.discount).to eq(discount1)
+      end
+    end
+  end
+
+  describe "class methods" do
     describe "#find_invoice_items" do
       it "returns all invoice_items associated with given invoice" do
 
